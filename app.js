31cf2110fa85ea6bf443331e3e6208bd8ee0c337
sleklere <santiagoleklere@gmail.com>
2022-09-06 16:39:51 -0300

    oficina martes

---
+++ app.js
@@ -0,0 +1,63 @@
+const tapir1 = document.querySelector(".tapir-1");
+const tapir2 = document.querySelector(".tapir-2");
+const tapir3 = document.querySelector(".tapir-3");
+const tapirs = document.querySelector(".tapirs");
+
+const tapirsEls = [tapir1, tapir2, tapir3];
+
+tapirsEls.forEach(function (tapir) {
+  let index = tapirsEls.indexOf(tapir) + 1;
+  tapirsEls.indexOf(tapir) == tapirsEls.length - 1 ? (index = 0) : null;
+  let tapirNext = tapirsEls[index];
+  tapir.addEventListener("click", function () {
+    tapir.classList.toggle("hidden");
+    tapirNext.classList.toggle("hidden");
+  });
+});
+
+const mintButton = document.querySelector(".btn-mint");
+const mintWindow = document.querySelector(".mint-window");
+const overlay = document.querySelector(".overlay");
+const closeMintButton = document.querySelector(".close-mint");
+const mintPrice = document.querySelector(".mint-price");
+const mintQuantity = Number(document.querySelector(".mint-quantity").value);
+const mintQuantity2 = document.querySelector(".mint-quantity");
+const mintQuantity3 = document.getElementById("mint-quantity");
+const noWl = document.querySelector(".no-wl");
+
+mintButton.addEventListener("click", function () {
+  mintWindow.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+});
+
+overlay.addEventListener("click", function () {
+  mintWindow.classList.add("hidden");
+  overlay.classList.add("hidden");
+});
+
+closeMintButton.addEventListener("click", function () {
+  mintWindow.classList.add("hidden");
+  overlay.classList.add("hidden");
+});
+
+// const num = 0;
+
+// if (num) {
+//   mintQuantity2.max = 1;
+// } else if (num == 0) {
+//   mintQuantity2.max = 0;
+//   noWl.classList.remove("hidden");
+// } else {
+//   mintQuantity2.max = 1;
+// }
+
+mintQuantity2.addEventListener("change", function () {
+  // mintPrice.textContent++;
+  if (this.value > 1) {
+    mintPrice.textContent = "0.006 Ξ";
+  } else if (this.value == 1) {
+    mintPrice.textContent = "0.003 Ξ";
+  } else {
+    mintPrice.textContent = 0.0;
+  }
+});
--- script.js
+++ script.js
@@ -1,82 +1,99 @@
-// const { MerkleTree } = require("merkletreejs");
-// const keccak256 = require("keccak256");
+let whitelistAddresses = [
+  "0x63Ca7A3F3c2984a286EB3be6afe011Ed6a5131df",
+  "0x3826335E2bc15Ffa99Bf697c28352C7E871a228b",
+];
 
-// let whitelistAddresses = [
-//   "0x63Ca7A3F3c2984a286EB3be6afe011Ed6a5131df",
-//   "0x3826335E2bc15Ffa99Bf697c28352C7E871a228b",
-// ];
+const leafNodes = whitelistAddresses.map((addr) => keccak256(addr));
+const merkleTree = new MerkleTree(leafNodes, keccak256, { sortPairs: true });
 
-// const leafNodes = whitelistAddresses.map((addr) => keccak256(addr));
-// const merkleTree = new MerkleTree(leafNodes, keccak256, { sortPairs: true });
+const rootHash = merkleTree.getRoot();
 
-// const rootHash = merkleTree.getRoot();
+const root =
+  "0x762cf6c3f961f9d5084b3fe2e3129ff762eb6bad898e3eef1348964a56e4722e";
 
-// console.log("Whitelist Merkle Tree\n", merkleTree.toString());
+console.log("Whitelist Merkle Tree\n", merkleTree.toString());
 
-// return;
+// console.log("the Merkle root is:", merkleTree.getRoot().toString("hex"));
 
-///////////////////////////////////
+/////////////////////////////
 
-const tapir1 = document.querySelector(".tapir-1");
-const tapir2 = document.querySelector(".tapir-2");
-const tapir3 = document.querySelector(".tapir-3");
-const tapirs = document.querySelector(".tapirs");
+function getMerkleProof(address) {
+  const hashedAddress = keccak256(address);
+  return merkleTree.getHexProof(hashedAddress);
+}
 
-const tapirsEls = [tapir1, tapir2, tapir3];
+const proof = getMerkleProof();
+// console.log(getMerkleProof("0x3826335E2bc15Ffa99Bf697c28352C7E871a228b"));
+// console.log(getMerkleProof("0x3826335E2bc15Ffa99Bf697c28352C7E871a228c"));
 
-tapirsEls.forEach(function (tapir) {
-  let index = tapirsEls.indexOf(tapir) + 1;
-  tapirsEls.indexOf(tapir) == tapirsEls.length - 1 ? (index = 0) : null;
-  let tapirNext = tapirsEls[index];
-  tapir.addEventListener("click", function () {
-    tapir.classList.toggle("hidden");
-    tapirNext.classList.toggle("hidden");
-  });
-});
+const buttonConnect = document.querySelector(".btn-connect-wallet");
 
-const mintButton = document.querySelector(".btn-mint");
-const mintWindow = document.querySelector(".mint-window");
-const overlay = document.querySelector(".overlay");
-const closeMintButton = document.querySelector(".close-mint");
-const mintPrice = document.querySelector(".mint-price");
-const mintQuantity = Number(document.querySelector(".mint-quantity").value);
-const mintQuantity2 = document.querySelector(".mint-quantity");
-const mintQuantity3 = document.getElementById("mint-quantity");
-const noWl = document.querySelector(".no-wl");
-
-mintButton.addEventListener("click", function () {
-  mintWindow.classList.remove("hidden");
-  overlay.classList.remove("hidden");
-});
-
-overlay.addEventListener("click", function () {
-  mintWindow.classList.add("hidden");
-  overlay.classList.add("hidden");
-});
-
-closeMintButton.addEventListener("click", function () {
-  mintWindow.classList.add("hidden");
-  overlay.classList.add("hidden");
-});
-
-// const num = 0;
-
-// if (num) {
-//   mintQuantity2.max = 1;
-// } else if (num == 0) {
-//   mintQuantity2.max = 0;
-//   noWl.classList.remove("hidden");
-// } else {
-//   mintQuantity2.max = 1;
-// }
-
-mintQuantity2.addEventListener("change", function () {
-  // mintPrice.textContent++;
-  if (this.value > 1) {
-    mintPrice.textContent = "0.006 Ξ";
-  } else if (this.value == 1) {
-    mintPrice.textContent = "0.003 Ξ";
+async function connect() {
+  if (window.ethereum) {
+    await window.ethereum.request({ method: "eth_requestAccounts" });
+    window.web3 = new Web3(window.ethereum);
+    const account = web3.eth.accounts;
+    //Get the current MetaMask selected/active wallet
+    const walletAddress = account.givenProvider.selectedAddress;
+    console.log(`Wallet: ${walletAddress}`);
+    web3.eth.getChainId().then(console.log);
   } else {
-    mintPrice.textContent = 0.0;
+    console.log("No wallet");
   }
+}
+const targetNetworkId = "0x4";
+
+const checkNetwork = async () => {
+  if (window.ethereum) {
+    const currentChainId = await window.ethereum.request({
+      method: "eth_chainId",
+    });
+
+    // return true if network id is the same
+    if (currentChainId == targetNetworkId) {
+      console.log("true");
+      return true;
+    }
+    // return false is network id is different
+    console.log("false");
+    return false;
+  }
+};
+
+// targets Rinkeby chain, id 4
+
+// switches network to the one provided
+const switchNetwork = async (correctNetwork) => {
+  if (!correctNetwork) {
+    console.log("incorrect network! changing now");
+    await window.ethereum.request({
+      method: "wallet_switchEthereumChain",
+      params: [{ chainId: targetNetworkId }],
+    });
+    // refresh
+    window.location.reload();
+  }
+};
+
+const checkAndSwitch = async () => {
+  const correctNetwork = await checkNetwork();
+  console.log(correctNetwork);
+  switchNetwork(correctNetwork);
+};
+
+buttonConnect.addEventListener("click", function () {
+  connect();
+  checkAndSwitch();
 });
+
+window.web3 = new Web3(window.ethereum);
+const account = web3.eth.accounts;
+//Get the current MetaMask selected/active wallet
+const walletAddress = account.givenProvider.selectedAddress;
+
+console.log(walletAddress);
+if (walletAddress != null) {
+  checkAndSwitch();
+}
+
+//////////////////////////
