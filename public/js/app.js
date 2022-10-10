ea49d0c8b0430a63dc58220a2885c872bb1169c8
sleklere <santiagoleklere@gmail.com>
2022-10-10 16:44:11 -0300

    updated wl's, update data interval, copy proof btn

--- public/js/app.js
+++ public/js/app.js
@@ -1,17 +1,17 @@
 const mintWindow = document.querySelector(".mint-window");
 const overlay = document.querySelector(".overlay");
 const closeMintButton = document.querySelector(".close-mint");
-// const mintPrice = document.querySelector(".mint-price");
 
-const tapir1 = document.querySelector(".tapir-1");
-const tapir2 = document.querySelector(".tapir-2");
-const tapir3 = document.querySelector(".tapir-3");
-const tapirsEls = [tapir1, tapir2, tapir3];
+let tapirs = [];
+// i < amount of tapirs
+for (let i = 1; i < 8; i++) {
+  tapirs.push(document.querySelector(`.tapir-${i}`));
+}
 
-tapirsEls.forEach(function (tapir) {
-  let index = tapirsEls.indexOf(tapir) + 1;
-  tapirsEls.indexOf(tapir) == tapirsEls.length - 1 ? (index = 0) : null;
-  let tapirNext = tapirsEls[index];
+tapirs.forEach(function (tapir, i, arr) {
+  let index = i + 1;
+  i == arr.length - 1 ? (index = 0) : null;
+  let tapirNext = tapirs[index];
   tapir.addEventListener("click", function () {
     tapir.classList.toggle("hidden");
     tapirNext.classList.toggle("hidden");
@@ -27,11 +27,3 @@ closeMintButton.addEventListener("click", function () {
   mintWindow.classList.add("hidden");
   overlay.classList.add("hidden");
 });
-
-// mintQuantity.addEventListener("change", function () {
-//   if (this.value > 1) {
-//     mintPrice.textContent = "0.003 Ξ";
-//   } else if (this.value == 1) {
-//     mintPrice.textContent = "Free!";
-//   }
-// });
